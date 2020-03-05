404c647f3e776523b64ab2946214e64dfdb0c7b4
Vladimir Ryabchenkov <vladimirryabchenkov@MacBook-Pro-Vladimir-2.local>
2020-03-05 16:24:04 +0300

    add tkinter

--- 20200303/app.py
+++ 20200303/app.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+
+
 F = Frame(borderwidth = 2, relief = "ridge")
 F.master.columnconfigure(0, weight = 1)
 F.master.rowconfigure(0, weight = 1)
@@ -13,5 +15,6 @@ B2 = Button (master=F, text = "Button2")
 
 B1.grid(sticky = "NEWS")
 B2.grid(sticky = "NEWS", column = 1, row = 0)
+
 
 mainloop()

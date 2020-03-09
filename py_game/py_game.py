ba96e6b3c630f6e1fb9e0de9b6865a709eedf7dd
Vladimir Ryabchenkov <vladimirryabchenkov@MacBook-Pro-Vladimir-2.local>
2020-03-09 18:16:24 +0300

    draw line on rectangles

--- py_game/py_game.py
+++ py_game/py_game.py
@@ -2,7 +2,7 @@ import pygame
 from random import randint
 
 def randcolor():
-	return (randint(100,256), randint(100,256),randint(100,256))
+	return (randint(100,255), randint(100,255),randint(100,255))
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -18,18 +18,25 @@ while True:
 	if e.type is pygame.QUIT:
 		print("QUIT")
 		break
-	if e.type is pygame.MOUSEBUTTONDOWN:
-		if e.button == 3:
-			windows.append((nwin, randcolor(), pygame.Rect(e.pos, SZ)))
-			nwin += 1
+	if e.type is pygame.MOUSEBUTTONDOWN and e.button == 3:
+		windows.append([nwin, randcolor(), pygame.Rect(e.pos, SZ), False])
+		nwin += 1
 	else:
-		for (i, color, rect) in reversed(windows):
-			if hasattr(e, "pos") and rect.collidepoint(e.pos):
+		for (i, color, rect, flag) in reversed(windows):
+			if e.type == pygame.MOUSEBUTTONUP and e.button == 1 and rect.collidepoint(e.pos):
+				windows[i][3] = True
+				print(f"{e} to {i}")
+				break
+			elif hasattr(e, "pos") and rect.collidepoint(e.pos):
 				print(f"{e} to {i}")
 				break
 		else:
 			print(e)
 	screen.fill(0)
-	for i, color, rect in windows:
+	for i, color, rect, flag in windows:
 		screen.fill(color, rect)
+		if flag:
+			pygame.draw.line(screen, (255,0,0), rect[:2], (rect[0]+100, rect[1]+80), 4)
+
+		
 	pygame.display.flip()

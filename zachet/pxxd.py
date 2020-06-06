bdccf3fc0638d3d873dc216098592f100e3dd370
vultar150 <vultar1500@gmail.com>
2020-06-06 17:31:39 +0300

    add option undo + redo

--- zachet/pxxd.py
+++ zachet/pxxd.py
@@ -6,6 +6,18 @@ from tkinter import filedialog as fd
 from tkinter import messagebox as mb
 
 
+def undo():
+    try:
+        text.edit_undo()
+    except Exception as ex:
+        mb.showinfo("Error", ex)
+
+def redo():
+    try:
+        text.edit_redo()
+    except Exception as ex:
+        mb.showinfo("Error", ex)
+
 def open_file():
     global open_name
     try:
@@ -14,8 +26,8 @@ def open_file():
         output = subprocess.check_output(["xxd", "-g1", open_name])
         text.delete(1.0, END)
         text.insert(1.0, output)
-    except:
-        mb.showinfo("Error", "Something wrong")
+    except Exception as ex:
+        mb.showinfo("Error", ex)
 
 
 def save_text():
@@ -32,8 +44,8 @@ def save_text():
             f = open(open_name, "wb")
         f.write(output)
         f.close()
-    except:
-        mb.showinfo("Error", "Something wrong")
+    except Exception as ex:
+        mb.showinfo("Error", ex)
 
 
 def save_text_as():
@@ -48,15 +60,15 @@ def save_text_as():
         f = open(save_name, "wb")
         f.write(output)
         f.close()
-    except:
-        mb.showinfo("Error", "Something wrong")
-
+    except Exception as ex:
+        mb.showinfo("Error", ex)
 
 
 root = Tk()
 
-text = Text(width=60, height=20)
-text.pack(side=LEFT)
+text = Text(width=80, height=40, undo=True)
+text.grid(row=0, column=1, sticky="NEWS")
+text.pack()
 
 scroll = Scrollbar(command=text.yview)
 scroll.pack(side=LEFT, fill=Y)
@@ -75,12 +87,18 @@ elif len(sys.argv) == 3:
     save_name = sys.argv[2]
 
 b_open = Button(frame, text="Open", width=10, command=open_file)
-b_open.grid(row=0, column=0)
+b_open.grid(row=1, column=0, sticky='NEWS')
 
 b_save = Button(frame, text="Save", width=10, command=save_text)
-b_save.grid(row=0, column=1)
+b_save.grid(row=1, column=1, sticky='NEWS')
 
 b_save_as = Button(frame, text="Save as", width=10, command=save_text_as)
-b_save_as.grid(row=0, column=2)
+b_save_as.grid(row=1, column=2, sticky='NEWS')
+
+b_undo = Button(frame, text="Undo", width=10, command=undo)
+b_undo.grid(row=2, column=1, sticky='NEWS')
+
+b_redo = Button(frame, text="Redo", width=10, command=redo)
+b_redo.grid(row=2, column=2, sticky='NEWS')
  
 root.mainloop()
